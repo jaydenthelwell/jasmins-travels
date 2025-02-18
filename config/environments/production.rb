81854b54ae4e797a1ba3ff1282b946240900c495
jaydenthelwell <jaydenthelwell02@gmail.com>
2025-02-18 19:29:06 +0000

    Precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,10 +13,9 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   config.eager_load = true
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.compile = false
+  config.public_file_server.enabled = true
   config.assets.digest = true
-
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
