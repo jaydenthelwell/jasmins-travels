5144059a0a6ed0838c6f58212f1cf99a351b3226
jaydenthelwell <jaydenthelwell02@gmail.com>
2025-02-19 11:06:31 +0000

    Mapbox

--- app/javascript/mapbox.js
+++ app/javascript/mapbox.js
@@ -1,28 +1,25 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiamF5ZGVudGhlbHdlbGwwMiIsImEiOiJjbTZ0cndyaTgwNXhiMmpzaDhjdncwaDVwIn0.vwESy6q4sxuGiNrK-GbC7g';
 
+
 const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-0.016, 51.576],
-  zoom: 12
+  style: 'mapbox://styles/jaydenthelwell02/cm7bs2tai006k01s8figgfal0',
+  center: [105.3188, 15.8700],
+  zoom: 2.5,
+  interactive: false,
+
 });
 
-new mapboxgl.Marker()
-  .setLngLat([-0.016, 51.576])
-  .addTo(map);
-
-  const locations = [
-    { name: 'Wanstead', coords: [0.0250, 51.5768] },
-    { name: 'Snaresbrook', coords: [0.022238, 51.58507] },
-    { name: 'Aldersbrook', coords: [0.0335, 51.5628] },
-    { name: 'South Woodford', coords: [0.0265, 51.5913] },
-    { name: 'Whipps Cross', coords: [0.0001, 51.5815] },
-    { name: 'Leytonstone', coords: [0.006519258113126995, 51.56969257331007] },
-    { name: 'Leyton', coords: [-0.005740, 51.573910] },
-    { name: 'Woodford Green', coords: [0.040895381189515065, 51.609893526988145] },
-    { name: 'Woodford', coords: [0.0325, 51.6042] }
-  ];
-
+const locations = [
+    { name: 'North Thailand', coords: [99.963, 19.432] },
+    { name: 'Vietnam', coords: [108.2772, 14.0583] },
+    { name: 'Cambodia', coords: [104.9910, 12.5657] },
+    { name: 'Japan', coords: [138.2529, 36.2048] },
+    { name: 'Philippines', coords: [121.7740, 12.8797] },
+    { name: 'Bali', coords: [115.1889, -8.4095] },
+    { name: 'Singapore', coords: [103.8198, 1.3521] },
+    { name: 'South Thailand', coords: [100.5231, 7.878978] }
+];
 
 locations.forEach(location => {
   new mapboxgl.Marker()
