81c3d04e6503877f36f3df8f80d3b986e9e5aba1
jaydenthelwell <jaydenthelwell02@gmail.com>
2025-02-24 21:06:18 +0000

    Logo

--- app/javascript/mapbox.js
+++ app/javascript/mapbox.js
@@ -4,8 +4,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiamF5ZGVudGhlbHdlbGwwMiIsImEiOiJjbTZ0cndyaTgwN
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/jaydenthelwell02/cm7bs2tai006k01s8figgfal0',
-  center: [120.3188, 16.00],
-  zoom: 2.7,
+  center: [120.3188, 8.00],
+  zoom: 3.3,
   interactive: false,
 
 });
@@ -14,7 +14,6 @@ const locations = [
     { name: 'North Thailand', coords: [99.963, 19.432], img: 'north-thailand.png' },
     { name: 'Vietnam', coords: [108.2772, 14.0583], img: 'north-thailand.png'  },
     { name: 'Cambodia', coords: [104.9910, 12.5657], img: 'north-thailand.png'  },
-    { name: 'Japan', coords: [138.2529, 36.2048], img: 'north-thailand.png'  },
     { name: 'Philippines', coords: [121.7740, 12.8797], img: 'north-thailand.png'  },
     { name: 'Bali', coords: [115.1889, -8.4095], img: 'north-thailand.png'  },
     { name: 'Singapore', coords: [103.8198, 1.3521], img: 'north-thailand.png'  },
