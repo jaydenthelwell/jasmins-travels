f2eb6926ec51d16cad236487a3516a993376c25f
jaydenthelwell <jaydenthelwell02@gmail.com>
2025-02-27 09:47:23 +0000

    Precompile fonts

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,11 @@
 # config/importmap.rb
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "popper", to: "popper.js", preload: true
-pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "background", to: "background.js"
-pin "mapbox", to: "mapbox.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin 'application'
+pin '@hotwired/turbo-rails', to: 'turbo.min.js'
+pin '@hotwired/stimulus', to: 'stimulus.min.js'
+pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
+pin 'popper', to: 'popper.js', preload: true
+pin 'bootstrap', to: 'bootstrap.min.js', preload: true
+pin 'background', to: 'background.js'
+pin 'background', to: 'video.js'
+pin 'mapbox', to: 'mapbox.js'
+pin_all_from 'app/javascript/controllers', under: 'controllers'
