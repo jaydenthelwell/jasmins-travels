ed1291b5392ded0122f078b60ca4eb7f50348367
jaydenthelwell <jaydenthelwell02@gmail.com>
2025-03-02 17:27:07 +0000

    Precompile fonts

--- app/javascript/video.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let video = document.querySelector("video");
-  video.muted = true;
-  video.play().catch(error => console.log("Autoplay failed:", error));
-});
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,5 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin 'popper', to: 'popper.js', preload: true
 pin 'bootstrap', to: 'bootstrap.min.js', preload: true
 pin 'background', to: 'background.js'
-pin 'video', to: 'video.js'
 pin 'mapbox', to: 'mapbox.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
