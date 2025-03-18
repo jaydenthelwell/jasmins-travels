f5fb6b673a82fcbc30c9477241cd624c3550f837
jaydenthelwell <jaydenthelwell02@gmail.com>
2025-03-18 16:35:29 +0000

    Precompile fonts

--- config/initializers/contentful.rb
+++ config/initializers/contentful.rb
@@ -1,9 +1,9 @@
-require "contentful"
+require 'contentful'
 
 ContentfulClient = Contentful::Client.new(
-  space: "qvwo3s6zz3y2",
-  access_token: "gf2Pskk-4gfnzt2T54F1LZSwD52uLnckrm1IrMcHXfM",
-  environment: "master",
+  space: 'efoudtk79op7',
+  access_token: 'tkRInxckprIc1voBgsn_OS8Obd1JBJ458Oy23vtkHBA',
+  environment: 'master',
   dynamic_entries: :auto,
   raise_errors: true
 )
